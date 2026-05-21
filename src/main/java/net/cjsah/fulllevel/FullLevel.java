b23e5e82d73c20ac27c47ebe9f58e1e73b0d0f0a
Cjsah <cjsah@cjsah.net>
2026-05-21 14:03:53 +0800

    init

---
+++ src/main/java/net/cjsah/fulllevel/FullLevel.java
@@ -0,0 +1,10 @@
+package net.cjsah.fulllevel;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FullLevel implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
